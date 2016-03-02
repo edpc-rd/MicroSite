d470305987969481ee6c2c448b74ed68af5e47fc
LinHuiMin <lam.waiman@qq.com>
2016-03-02 14:55:52 +0800

    Rename MicroSite

--- config/app.php
+++ config/app.php
@@ -22,7 +22,7 @@ return [
 	|
 	| The application name for use within the UI of the application
 	*/
-    'name' => 'JSWInfocenter',
+    'name' => 'MicroSite',
 
     /*
     |--------------------------------------------------------------------------
