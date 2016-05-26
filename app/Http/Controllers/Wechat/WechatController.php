dab2fa2e6e152e19f6124d23dba7ce104b874017
LinHuiMin <lam.waiman@qq.com>
2016-05-26 10:06:40 +0800

    Add WeChat Support

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -18,7 +18,7 @@ class WechatController extends Controller
      *
      * @return string
      */
-    public function serve()
+    public function serve_test()
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
@@ -31,4 +31,32 @@ class WechatController extends Controller
 
         return $wechat->server->serve();
     }
+
+    public function serve()
+    {
+        include_once "WXBizMsgCrypt.php";
+
+        // 假设企业号在公众平台上设置的参数如下
+        $encodingAesKey = "yyaNeacqghYBasSEy4d4InUPwTCIbAkyf15fJ4CIfw3";
+        $token = "aRAXM84VRIAq8Y3";
+        $corpId = "aRAXM84VRIAq8Y3";//需要更换
+
+
+        $sVerifyMsgSig =$_GET["msg_signature"];
+        $sVerifyTimeStamp =$_GET["timestamp"];
+        $sVerifyNonce = $_GET["nonce"];
+        $sVerifyEchoStr = $_GET["echostr"];
+        // 需要返回的明文
+        $sEchoStr = "";
+        $wxcpt = new WXBizMsgCrypt($token, $encodingAesKey, $corpId);
+        $errCode = $wxcpt->VerifyURL($sVerifyMsgSig, $sVerifyTimeStamp, $sVerifyNonce, $sVerifyEchoStr, $sEchoStr);
+        if ($errCode == 0) {
+
+            echo $sEchoStr;
+
+        } else {
+            print("ERR: " . $errCode . "\n\n");
+        }
+    }
+
 }
